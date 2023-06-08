c6537bd908e8d16b5ca7b1d923d7e6c928ff0d71
kuche1 <znamsiparolata4esno@abv.bg>
2023-06-08 21:56:45 +0300

    update

--- osv3/common.h
+++ osv3/common.h
@@ -11,6 +11,17 @@
 
 #define put(text) Disp text
 
+#define str_len(str) length(str)
+
+/////////////////////////////////////// shorthands
+
+////////// string; 0-indexed
+// TODO untested
+
+#define str_tail(str, first_tail_item_idx) sub(str,first_tail_item_idx+1,str_len(str)-first_tail_item_idx)
+#define str_head(str, last_head_item_idx) sub(str,1,last_head_item_idx+1)
+#define str_cut_idx(str, start_idx, end_idx) sub(str,start_idx+1,end_idx-start_idx+1)
+
 /////////////////////////////////////// programs
 
 #define push(var) CONCAT(prgmPUSH, var)
